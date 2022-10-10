a143d0b6c0f44cda1e5e8d4bfe81f0d743bad559
Gokul Rithik <rithikgokul200@gmail.com>
2022-10-10 16:18:49 +0530

    parallax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import aos from "aos"
+import forest from "./bulb.jpg"
 
-function App() {
+export default function App() {
+
+    useEffect(() => {
+        aos.init()
+    },[])
+
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className="wrapper">
+            <section >
+                <h1 data-aos ="fade-in">Welcome this is Parallax Scrolling</h1>
+            </section>
+            <header>
+                <img src={forest} alt="forest" className='background' />
+            </header>
+            <section >
+                <h1>Image above moves slower than the other elements in the page because it is far from the screen then it actually is</h1>
+            </section>
+        </div>
+            
     </div>
-  );
+  )
 }
-
-export default App;
